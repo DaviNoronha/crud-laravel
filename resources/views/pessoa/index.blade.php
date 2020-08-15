a4950978bf916ea85b16c5f2a9952329f0955c9c
Davi Noronha <davinoronha.com@gmail.com>
2020-08-15 15:23:34 -0400

    Tabela de Pessoas Estilizada

--- resources/views/pessoa/index.blade.php
+++ resources/views/pessoa/index.blade.php
@@ -1,12 +1,34 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <h1>Pessoas Cadastradas</h1>
 
-<a href="/pessoas/create">Cadastrar nova pessoa</a>
+<a href="/pessoas/create" class="btn btn-success">Cadastrar nova pessoa</a>
 
 <hr>
 
-@foreach($pessoas as $p)
-    <p>Nome: {{ $p->nome}} </p>
-    <p>Telefone: {{ $p->telefone}}</p>
-    <p>Email: {{ $p->email}}</p>
-<hr>
-@endforeach
+<table class="table table-bordered table-dark">
+    <tr>
+        <th>Nome</th>
+        <th>Telefone</th>
+        <th>Email</th>
+        <th>Ações</th>
+    </tr>
+    @foreach($pessoas as $p)
+    <tr>
+        <td>{{ $p->nome}}</td>
+        <td>{{ $p->telefone}}</td>
+        <td>{{ $p->email}}</td>
+        <td>---</td>
+    </tr>
+    @endforeach
+</table>
+
+<style>
+html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+</style>
