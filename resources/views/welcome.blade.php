ae84cc7096072baa27a20ea56efeeef5656c066d
Davi Noronha <davinoronha.com@gmail.com>
2020-08-15 14:46:14 -0400

    davi teste gitkraken

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Davi Laravel
+                    Davi_Laravel
                 </div>
 
                 <div class="links">
